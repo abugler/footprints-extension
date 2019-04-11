0bea204a06fc81b9e1cff89ead559eaca9bd8df1
abugler <abugler@gmail.com>
2019-04-11 09:42:21 -0500

    "attempted to put a notice about what issues are locked, committing changes, unfinished"

--- js/keep_phone_number.js
+++ js/keep_phone_number.js
@@ -1,7 +1,25 @@
+let inputs = document.getElementsByClassName("ticketField");
+
+
+for (let i = 0; i<inputs.length; i++){
+    inputs[i].onkeypress = "return myCheckKey()";
+}
+
+function myCheckKey(e)
+{
+    let andor = document.getElementsByName("ANDOR");
+    if (andor[1] != null)
+        andor[1].checked = true;
+    checkKey(e);
+}
+
+/**
 let andor = document.getElementsByName("ANDOR");
-andor.checked[1] = true;
-andor.checked[0] = false;
+if (andor[1] != null)
+    andor[1].checked = true;
+**/
 
 // This code will allow the user to keep the phone number that copies over from interaction desktop
 // Before the user had to cut the phone number out, enter the NetID, and paste the phone number back into contacts
 // Now the User just needs to put in the NetID. Yay!
+
--- js/select_contact.js
+++ js/select_contact.js
@@ -1,3 +1,4 @@
+
 function updateValues() {
   console.log("SWITCHING SEARCH TYPE");
   
